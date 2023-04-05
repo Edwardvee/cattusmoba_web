7e3ac76de1613b441066135c3252c5e0b049577d
nicolas-k-cmd <nicokrule@gmail.com>
2023-04-04 23:44:25 -0300

    Ahora los usuarios utilizan UUID

--- kyatsu/app/Models/User.php
+++ kyatsu/app/Models/User.php
@@ -12,6 +12,7 @@ class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
 
+    protected $primary_key = "uuid";
     /**
      * The attributes that are mass assignable.
      *
