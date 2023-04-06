54591c6c6ee4673c77bec2d62567dea1190a4e62
lucas <lucas.boghossian.et@gmail.com>
2023-04-06 01:20:55 -0300

    manuel xd

---
+++ kyatsu/resources/views/como jugar.blade.php
@@ -0,0 +1,13 @@
+@extends("maintemplate")
+
+@section("http_body")
+
+
+<head>
+  <title>Kyatsu! - Cómo jugar</title>
+</head>
+
+<body>
+  <p>Este es la pagina de Cómo jugar</p>
+
+  @endsection("http_body")
