9c562edf6dc2bac1280a58098fde50a6a779a630
nicolas-k-cmd <nicokrule@gmail.com>
2023-04-12 22:12:16 -0300

    Agregar heroes a la database

--- kyatsu/database/factories/UserFactory.php
+++ kyatsu/database/factories/UserFactory.php
@@ -21,7 +21,6 @@ class UserFactory extends Factory
     public function definition(): array
     {
         return [
-            
             'name' => fake()->name(),
             'email' => fake()->unique()->safeEmail(),
             'email_verified_at' => now(),
---
+++ kyatsu/database/migrations/2023_04_12_230754_heroes.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create("heroes", function (Blueprint $table) {
+            $table->uuid();
+            $table->string("name", 32)->unique();
+            $table->text("description");
+            $table->timestamps();
+            $table->softDeletes($column = 'deleted_at', $precision = 0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('heroes');
+    }
+};
