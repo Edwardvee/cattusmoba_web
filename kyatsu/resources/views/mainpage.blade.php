6b1b1b8c933b6a7643c16ce4ebb31e6fbd8d9234
Manhuel_Et26 <manuel.casimiro.et26@gmail.com>
2023-04-17 12:36:04 -0300

    Cambios realizados a descripcion de personaje en Mainpage

--- kyatsu/resources/views/mainpage.blade.php
+++ kyatsu/resources/views/mainpage.blade.php
@@ -11,6 +11,7 @@
 
 <body>
   
+  
   <div><!--  aunque no tenga sentido esto lo centra ._. --> 
  <header>
     <h1 class="img-title">Kyatsu! Moba</h1>
@@ -19,8 +20,14 @@
   </header>
 </div>
 
-<div class="redes rounded-pill">
-  <p class="OurRed"> Nuestras redes: </p>
+<div class="redes">
+  <p class="OurRed"> Siguenos en nuestras redes sociales</p>
+  <br> 
+    <img class="Red" src="img/Discord.png" width="40px"> 
+    <img class="Red" src="img/YouTube.png" width="40px">
+    <img class="Red" src="img/Facebook.png" width="40px">
+    <img class="Red" src="img/Instagram.png" width="40px">
+
   
 </div>
 
@@ -31,12 +38,18 @@
  <div class="card mb-3" style="max-width: 100%;">
   <div class="row g-0">
     <div class="col-md-4">
-      <img src="img/placeholder2.png" class="img-fluid rounded-start" alt="...">
+      <img id="myImage" src="img/placeholder2.png" class="img-fluid rounded-start" alt="...">
     </div>
   <div class="col-md-8 CrdRightSide">
       <div class="card-body">
         <h5 class="card-title cardTitle">Heroes increibles</h5>
-        <p class="card-text cardText">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text cardText">Ya sea que prefieras lanzarte directo a la batalla, apoyar a tus compañeros de equipo, o algo intermedio, hay un lugar para ti en la arena</p>
+      <div class="col-md-3">
+        <button class="heroesDesc" onclick=dañojs(); id="daño">Daño</button>
+        <button class="heroesDesc" onclick=soportejs(); id="soporte">Soporte</button>
+        <button class="heroesDesc" onclick=tanquejs(); id="tanque">Tanque</button>
+      </div>
+        <p class="Descripcion" id="message">Los daños se enfocan claramente en hacer daño.</p>
       </div>
     </div>
 
@@ -46,18 +59,7 @@
   <div>
   </div>
 </div>
-    <h1> Esta es la main page</h1>
-    <h1> Esta es la main page</h1>
-    <h1> Esta es la main page</h1>
-    <h1> Esta es la main page</h1>
-    <h1> Esta es la main page</h1>
-    <h1> Esta es la main page</h1>
-    <h1> Esta es la main page</h1>
-    <h1> Esta es la main page</h1>
-    <h1> Esta es la main page</h1>
-    <h1> Esta es la main page</h1>   
-    <h1> Esta es la main page</h1>
-
+  
 
 </div>
 </body>
@@ -68,6 +70,24 @@ var rand = Math.floor(Math.random()*MPtext.length);
 var randtext = MPtext[rand];
 document.getElementById("rrandommsg").innerHTML = randtext;
 
+function tanquejs(){
+  document.getElementById('myImage')
+    .src="img/cait.png";
+    document.getElementById('message')
+    .innerHTML="Los tanques claramente se centran en tanquear el daño y proteger a sus compañeros";
+  }
+    function soportejs(){
+  document.getElementById('myImage')
+    .src="img/india.jpg";
+    document.getElementById('message')
+    .innerHTML="Los soportes se centran en apoyar al equipo ya sea protegeiendo a sus compañeros, curandolos o potenciandolos";
+  }
+    function dañojs(){
+  document.getElementById('myImage')
+    .src="img/placeholder2.png";
+    document.getElementById('message')
+    .innerHTML="Los daños se enfocan claramente en hacer daño";
+}
 </script>
 @endsection("http_body")
 
