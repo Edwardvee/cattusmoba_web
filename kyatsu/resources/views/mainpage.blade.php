8597e857ce2700579a286e97c29f8da3d858f7f4
Manhuel_Et26 <manuel.casimiro.et26@gmail.com>
2023-04-19 22:42:26 -0300

    Siendo un genio como siempre (mejore el apartado de heroes)

--- kyatsu/resources/views/mainpage.blade.php
+++ kyatsu/resources/views/mainpage.blade.php
@@ -38,16 +38,16 @@
  <div class="card mb-3" style="max-width: 100%;">
   <div class="row g-0">
     <div class="col-md-4">
-      <img id="myImage" src="img/placeholder2.png" class="img-fluid rounded-start" alt="...">
+      <img id="myImage" src="img/cait.png" class="img-fluid rounded-start heroImg" alt="...">
     </div>
   <div class="col-md-8 CrdRightSide">
       <div class="card-body">
         <h5 class="card-title cardTitle">Heroes increibles</h5>
         <p class="card-text cardText">Ya sea que prefieras lanzarte directo a la batalla, apoyar a tus compañeros de equipo, o algo intermedio, hay un lugar para ti en la arena</p>
-      <div class="col-md-3">
-        <button class="heroesDesc" onclick=dañojs(); id="daño">Daño</button>
-        <button class="heroesDesc" onclick=soportejs(); id="soporte">Soporte</button>
-        <button class="heroesDesc" onclick=tanquejs(); id="tanque">Tanque</button>
+      <div class="col-md-12">
+        <button class="heroesDesc" onclick="cambiarImagen('img/cait.png', 'Texto de la imagen 1')">Daño<img class="butIcoHero" src="img/damage.png"></button>
+        <button class="heroesDesc" onclick="cambiarImagen('img/india.jpg', 'Texto de la imagen 2')">Soporte</button>
+        <button class="heroesDesc" onclick="cambiarImagen('img/placeholder2.png', 'Texto de la imagen 3')">Tanque</button>
       </div>
         <p class="Descripcion" id="message">Los daños se enfocan claramente en hacer daño.</p>
       </div>
@@ -70,24 +70,19 @@ var rand = Math.floor(Math.random()*MPtext.length);
 var randtext = MPtext[rand];
 document.getElementById("rrandommsg").innerHTML = randtext;
 
-function tanquejs(){
-  document.getElementById('myImage')
-    .src="img/cait.png";
-    document.getElementById('message')
-    .innerHTML="Los tanques claramente se centran en tanquear el daño y proteger a sus compañeros";
-  }
-    function soportejs(){
-  document.getElementById('myImage')
-    .src="img/india.jpg";
-    document.getElementById('message')
-    .innerHTML="Los soportes se centran en apoyar al equipo ya sea protegeiendo a sus compañeros, curandolos o potenciandolos";
-  }
-    function dañojs(){
-  document.getElementById('myImage')
-    .src="img/placeholder2.png";
-    document.getElementById('message')
-    .innerHTML="Los daños se enfocan claramente en hacer daño";
+function cambiarImagen(src, mensaje) {
+  var imagen = document.getElementById("myImage");
+  var message = document.getElementById("message");
+  
+  imagen.style.opacity = 0;
+  
+  setTimeout(function() {
+    imagen.src = src;
+    message.textContent = mensaje;
+    imagen.style.opacity = 1;
+  }, 1000);
 }
+
 </script>
 @endsection("http_body")
 
