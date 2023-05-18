282d31918336d2026ed14239b15d3f1652deeca2
Edwardvee <lautaroivan.quiroga@gmail.com>
2023-05-18 00:20:43 -0300

    Continuando historia
    arreglando botones

--- kyatsu/resources/views/historia.blade.php
+++ kyatsu/resources/views/historia.blade.php
@@ -12,31 +12,35 @@
 <main>
 <div class="pepe">
    <section id="one">
-    <h1>oña</h1>
+    <h1>Ubicacion 1</h1>
    </section>
    <section id="two">
-    <h1>año</h1>
+    <h1>Ubicacion 2</h1>
    </section>
    <section id="three">
-    <h1>ñoa</h1>
+    <h1>Ubicacion 3</h1>
    </section>
    <section id="four">
-    <h1>aoñ</h1>
+    <h1>Ubicacion 4</h1>
    </section>
 
 
     <ul class="guide">
         <li>
-        <button id="one">oña</button> 
+            <p id="L1" onclick="scrollGo(one, this.id)"><i id="L1icon" class="bi bi-arrow-right-circle-fill iconoubi"></i> Ubicacion 1
+            </p> 
         </li>
         <li>
-        <p>oña</p>
+        <p id="L2" onclick="scrollGo(two, this.id)"><i id="L2icon" class="bi bi-arrow-right-circle iconoubi"></i>
+             Ubicacion 2</p>
             </li>
         <li>
-        <p>oña</p>      
+        <p id="L3" onclick="scrollGo(three, this.id)"><i id="L3icon"class="bi bi-arrow-right-circle iconoubi"></i>
+             Ubicacion 3</p>      
       </li>
         <li>
-        <p>oña</p>
+        <p id="L4" onclick="scrollGo(four, this.id)"><i id="L4icon" class="bi bi-arrow-right-circle iconoubi"></i>
+             Ubicacion 4</p>
     </li>
     </ul>
     
@@ -44,16 +48,23 @@
 </main>
 <script>
 
-$(document).ready(function (){
-   
-    var one = $("#one").position() ;
-    $("#one").(function(){
-       alert('g');
-    });
-
-    $("body").addClass("preventScroll");
-});
-
+function scrollGo(id, button){
+    id.scrollIntoView(false);
+    let iconos = document.getElementsByClassName("iconoubi");
+    for(i=0; i<iconos.length; i++){
+        iconos[i].classList.remove("bi-arrow-right-circle");
+        iconos[i].classList.remove("bi-arrow-right-circle-fill");
+        iconos[i].classList.add("bi-arrow-right-circle");
+    }
+    let iconmod = button + "icon";
+    let poop = document.getElementById( iconmod );
+    poop.classList.remove("bi-arrow-right-circle");
+    poop.classList.add("bi-arrow-right-circle-fill");
+       /*
+   const info = id.getBoundingClientRect();
+    scroll(info.x, info.y);
+     */
+}
 </script>
 @endsection("http_body")
 
