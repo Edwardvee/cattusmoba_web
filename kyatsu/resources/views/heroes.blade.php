f61123742625cd865c4081c7e0f398f3fc5b42b0
OGBiggieCheese <manuel.casimiro.et26@gmail.com>
2023-06-22 12:30:50 -0300

    holas

--- kyatsu/resources/views/heroes.blade.php
+++ kyatsu/resources/views/heroes.blade.php
@@ -12,22 +12,22 @@
 <div class="container">
    <div class="row">
      <div class="col"> <!-- descripcion de los heroes -->
-         <h1 class="sub-estetico">Melisa</h1>
+         <h1 id="heroname" class="sub-estetico">Melisa</h1>
           <div class="row">
             <div class="col-8 niggabackground">
                   <h3>Actriz de voz: Ivan Quiroga</h3>
                   <h6>Cumpleaños / 04-06</h6>
-                  <p>Lisa Minci es la bibliotecaria de los caballeros de Favonio de Mondstadt en Kyatsu! y, a pesar de su apariencia sosa, es un ser muy inteligente y una maga con increíble talento que en realidad se cohíbe por su temor al costo del conocimiento</p>
+                  <p class="message">Melisa es la bibliotecaria de los caballeros de Favonio de Mondstadt en Kyatsu! y, a pesar de su apariencia sosa, es un ser muy inteligente y una maga con increíble talento que en realidad se cohíbe por su temor al costo del conocimiento</p>
             </div> 
          </div>
       </div>
 
      <div class="col-6 pj-container"> <!-- imagenes de los heroes -->
-       <img class="pj" src="https://img1.picmix.com/output/stamp/normal/2/1/7/6/1866712_2dde6.png"> 
+       <img class="pj" src="{{url('img/melisa.png')}}"> 
      </div>
 
      <div class="col-12 align-self-end niggabackground"> <!--selector de heroes-->
-           <button class="heroes"></button>
+           <button class="heroes" onclick="cambiarImagen('https://media.tenor.com/2kBRRgoejx4AAAAi/league-of-legends.gif', 'Melisa es la bibliotecaria de los caballeros de Favonio de Mondstadt en Kyatsu! y, a pesar de su apariencia sosa, es un ser muy inteligente y una maga con increíble talento que en realidad se cohíbe por su temor al costo del conocimiento','myImage2', 'Holas')"></button>
            <button class="heroes"></button>
            <button class="heroes"></button>
            <button class="heroes"></button>
@@ -43,13 +43,15 @@
 </div>
 </body>
 <script>
-     function cambiarImagen(src, mensaje,img) {
+     function cambiarImagen(src, mensaje,img, mensaje2) {
                 var imagen = document.getElementById(img);
                 var message = document.getElementById("message");
+                var heroname = document.getElementById("mensaje2")
 
                 setTimeout(function() {
                     imagen.src = src;
                     message.textContent = mensaje;
+                    heroname.textContent = mensaje2;
                     imagen.classList.add("animate-image"); // Agrega la clase "animate-image"
                 }, 100);
                 imagen.classList.remove("animate-image");
