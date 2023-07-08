52e895334b73528940fc987faa5e23564c2de764
Edwardvee <lautaroivan.quiroga@gmail.com>
2023-07-07 23:37:12 -0300

    Pagina de parches automatizada
    usando python para agarrar las actualizaciones de la pagina de github y un fetch para meterlo a la pagina no hay que preocuparse de que no quede vacia

--- kyatsu/public/py/github_news.py
+++ kyatsu/public/py/github_news.py
@@ -10,8 +10,17 @@ for commit in commits:
      description = ""
      if commit.find('div', class_ = "my-2 Details-content--hidden"):
               description = commit.find('pre', class_ = "text-small ws-pre-wrap").text
-     final_output += "<div class='row'><div class='col'><p>"+ name +"</p><p>"+ description +"</p></div></div>"
+     final_output += "<div class='row dev-item-row'><div class='col info-col'><p class='name'>--"+ name +"</p><p class='description'>"+ description +"</p></div></div>"
 print(final_output)
+
+page2 = r.get("https://github.com/Edwardvee/kyatsumobagame/releases").content
+final_output2 = ""
+soup2 = BeautifulSoup(page2, 'lxml')
+releases = soup2.find_all('div', class_ = "col-md-9")
+for release in releases:
+     name = release.find('a', class_ = "Link--primary").text
+     description = release.find('p', class_ = "").text
+     final_output2 += "<div class='row dev-item-row'><div class='col info-col'><p class='name'>--"+ name +"</p><p class='description'>"+ description +"</p></div></div>"
 
 
 def main():
@@ -19,6 +28,11 @@ def main():
 
     with open('output.txt', 'w') as f:
         f.write(output)
+
+    output2 = final_output2  
+    with open('output2.txt', 'w') as f:
+        f.write(output2)
+
 
 if __name__ == '__main__':
     main()
