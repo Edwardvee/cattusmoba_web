494fd66741d48b8fdb5263748383aec9e1f23d9a
lucas <lucas.boghossian.et@gmail.com>
2023-09-08 17:04:50 -0300

    :v

--- kyatsu/resources/views/store.blade.php
+++ kyatsu/resources/views/store.blade.php
@@ -1,41 +1,78 @@
-@extends("maintemplate")
+  @extends("maintemplate")
 
-@section("http_body")
-<head>
-  <title>Kyatsu - Tienda</title>
-  <link rel="stylesheet" href="{{url('css/store.css')}}">
-
-</head>
-
-
-<div class="container store row mx-auto text-center">
-    <div class="col-md-3  me-auto" >
-    <img class="coin" src="{{url('img/coin.png')}}"> 
-  <p>1000 KP</p>
-   <p style="margin-top: -20px">K-Points</p>
-        <div class="price"> $7.99 </div>
+  @section("http_body")
+  <head>
+    <title>Kyatsu - Tienda</title>
+    <link rel="stylesheet" href="{{url('css/store.css')}}">
+  </head>
+  <br><br><br><br><br><br>  
+  <div class="container">
+  <div class="d-flex row">
+    <div class="col-sm-6 col-lg-3 item">
+    <div class="card shadow-sm" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">1 Nicoin</h5>
+      <p class="card-text">No seas rata y compra mas de 1</p>
+      <div class="d-flex justify-content-center">
+      <a href="#" class="align-self-end btn btn-primary">lo compro!</a>
+      </div>
     </div>
-    <div class="col-md-3 me-auto">
-    <img class="coin" src="{{url('img/coin.png')}}"> 
-        <p>2800 KP</p>
-    <p style="margin-top: -20px">K-Points</p>
-    <div class="price "> $19.99 </div>
+  </div>
+    </div>  
+    <div class="col-sm-6 col-lg-3 item">
+    <div class="card shadow-sm" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">50 Nicoins</h5>
+      <p class="card-text">50 Nicoins (no confunindirse con 50 sombras de grey)
+      </p>
+      <div class="d-flex justify-content-center">
+      <a href="#" class="align-self-end btn btn-primary">LO QUIERO!</a>
+      </div>
     </div>
-    <div class="col-md-3  me-auto">
-    <img class="coin" src="{{url('img/coin.png')}}"> 
-      <p>5000 KP</p> 
-     <p style="margin-top: -20px">K-Points</p>
-    <div class="price"> $31.99 </div>
+  </div>
     </div>
-    <div class="col-md-3  me-auto" >
-    <img class="coin" src="{{url('img/coin.png')}}"> 
-  <p>13500 KP</p>
-   <p style="margin-top: -20px">K-Points</p>
-        <div class="price"> $79.99 </div>
+
+    <div class="col-sm-6 col-lg-3  item">
+    <div class="card shadow-sm" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">500+2 Nicoins</h5>
+      <p class="card-text">te regalamos 2 Nicoins para que sepas que sos el 2 en todo</p>
+      <div class="d-flex justify-content-center">
+      <a href="#" class="align-self-end btn btn-primary">me lo llevo!</a>
+      </div>
     </div>
-  
+  </div>
+    </div>
 
-</div>
+    <div class="col-sm-6 col-lg-3 item">
+    <div class="card shadow-sm" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">5E+1321 Nicoins</h5>
+      <p class="card-text">Ahora el Nicoins vale menos que el peso pero mas que el dolar</p>
+      <div class="d-flex justify-content-center">
+      <a href="#" class="align-self-end btn btn-primary">sera mio!</a>
+      </div>
+    </div>
+  </div>
+    </div>
+  </div>
+  </div>
+    
 
 
-@endsection("http_body")
+
+  <!-- <div class="main-content">
+  <div class="content-page">
+  <div class="tittle-section">Objetos a la venta </div>
+  <div class="product-list">
+  <div class="product-box">
+    <a href="">
+      <div class="product">
+        <img img="cattusmoba_web/kyatsu/public/img/aborigen no">
+        <div class="detail-tittle">aaaaa</div>
+        <div class="detail-description">aaaaaaaaa</div>
+        <div class="detail-price">12 katumoneda <span>50</span></div>
+  </div>
+
+
+  -->
