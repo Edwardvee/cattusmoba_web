5f18eb8ed66f9eba83334fc5c74ddc77d13c37e8
Manhuel_Et26 <manuel.casimiro.et26@gmail.com>
2023-09-17 18:26:08 -0300

    Actualizacion de la pagina heroes
    Se han agregado animaciones extra para el texto, ademas de agregar en la base de datos la seccion de actor/riz del heroe

--- kyatsu/resources/views/heroes.blade.php
+++ kyatsu/resources/views/heroes.blade.php
@@ -18,6 +18,9 @@ $heroes = $heroes_all[$heroes];
   $(window).on("load", function(){
     $('#character').addClass('slide-in-right');
 });
+$(window).on("load", function(){
+    $('#description').addClass('scale-in-center');
+});
 </script>
 
 <body class="ola"> <!-- class ola para bajar el body 120px por el navbar-->
@@ -26,8 +29,8 @@ $heroes = $heroes_all[$heroes];
       <div class="col"> <!-- descripcion de los heroes -->
         <h1 id="heroname" class="sub-estetico">{{ $heroes["name"] }}</h1>
         <div class="row">
-          <div class="col-8 niggabackground">
-            <h3>Actriz de voz: Ivan Quiroga</h3>
+          <div id="description"  style="opacity: 0;" class="col-8 darkbg">
+            <h4>Actriz de voz: {{ $heroes['voice_actor'] }}</h4>
             <h6>Cumpleaños / 04-06</h6>
             <p class="message">{{ $heroes['description'] }}</p>
           </div>
@@ -38,49 +41,15 @@ $heroes = $heroes_all[$heroes];
         <img id="character" style="opacity: 0;" class="pj" src="{{url('img/'.  $heroes['name'] . '.png')}} ">
       </div>
 
-      <div class="col-12 align-self-end niggabackground"> <!--selector de heroes-->
-        <!-- <button class="heroes" onclick="cambiarImagen('https://media.tenor.com/2kBRRgoejx4AAAAi/league-of-legends.gif', 'Melisa es la bibliotecaria de los caballeros de Favonio de Mondstadt en Kyatsu! y, a pesar de su apariencia sosa, es un ser muy inteligente y una maga con increíble talento que en realidad se cohíbe por su temor al costo del conocimiento','myImage2', 'Holas')"></button>
--->
+      <div class="col-12 align-self-end darkbg"> <!--selector de heroes-->
         @php
         foreach($heroes_all as $hero_ind){
-
         echo "<a href='" . $hero_ind['name'] . "'><button class='heroes' id='" . $hero_ind['name'] . "' style='background-image: url(../img/" . $hero_ind['name'] ."alt". ".png)'></button></a>" ; }; @endphp </div>
-
       </div>
     </div>
 </body>
 
 <style>
-.slide-in-right {
-  opacity: 1;
-	-webkit-animation: slide-in-right 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-	        animation: slide-in-right 0.5s cubic-bezier(0.250, 0.460, 0.450, 0.940) both;
-}
-
-@-webkit-keyframes slide-in-right {
-  0% {
-    -webkit-transform: translateX(1000px);
-            transform: translateX(1000px);
-    opacity: 0;
-  }
-  100% {
-    -webkit-transform: translateX(0);
-            transform: translateX(0);
-    opacity: 1;
-  }
-}
-@keyframes slide-in-right {
-  0% {
-    -webkit-transform: translateX(1000px);
-            transform: translateX(1000px);
-    opacity: 0;
-  }
-  100% {
-    -webkit-transform: translateX(0);
-            transform: translateX(0);
-    opacity: 1;
-  }
-}
 
 </style>
 
