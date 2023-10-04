be601c0b09490865552c7e83803be70a12d6c94d
nicolas-k-cmd <nicokrule@gmail.com>
2023-10-04 16:24:38 -0300

    Reparar edicion de usuarios

--- kyatsu/resources/views/admin/editusers.blade.php
+++ kyatsu/resources/views/admin/editusers.blade.php
@@ -17,6 +17,15 @@
 <br>
 <div class="container">
     <link rel="stylesheet" href="{{url('css/usershow.css')}}">
+    @if($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
     <div class="card">
         <div class="card-body">
             <form method="POST" , action="{{route('admin.admin_users.update', ['admin_user' => $user['uuid']])}}">
@@ -25,16 +34,16 @@
                 <div class="row">
                     <div class="mb-2 col-12">
                     <p class="text-center">UUID:</p>
-                        <input type="text" class="form-control" id="uuid" readonly placeholder="{{$user['uuid']}}" disabled></input>
+                        <input type="text" name="uuid" class="form-control" id="uuid" readonly value="{{$user['uuid']}}"></input>
                     </div>
                     <div class="mb-2 col-12">
                     <p class="text-center">E-mail:</p>
 
-                        <input type="email" class="form-control" id="email" value="{{$user['email']}}"></input>
+                        <input type="email" name="email" class="form-control" id="email" value="{{$user['email']}}"></input>
                     </div>
                     <div class="mb-2 col-12">
                     <p class="text-center">Nombre de usuario:</p>
-                        <input type="text" class="form-control" id="name" value="{{$user['name']}}"></input>
+                        <input type="text" name="name" class="form-control" id="name" value="{{$user['name']}}"></input>
                     </div>
                     <div class="row justify-content-center">
                     <div class="mt-4 col-auto">
