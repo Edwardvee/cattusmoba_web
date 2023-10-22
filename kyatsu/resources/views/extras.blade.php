ff8d7bf8409203e12612d5e26b20830939ba88d5
Manhuel_Et26 <manuel.casimiro.et26@gmail.com>
2023-10-21 21:00:36 -0300

    Ta te ti uwu

--- kyatsu/resources/views/extras.blade.php
+++ kyatsu/resources/views/extras.blade.php
@@ -5,20 +5,30 @@
 <body>
   <div class="d-flex text-center">
     <h1 class="text-center">Ta-Te-Ti</h1>
-    <center>
-      <div id="board">
-        <div class="cells player"></div>
-        <div class="cells player"></div>
-        <div class="cells player"></div>
-        <div class="cells player"></div>
-        <div class="cells player"></div>
-        <div class="cells player"></div>
-        <div class="cells player"></div>
-        <div class="cells player"></div>
-        <div class="cells player"></div>
-        <button id="reset-button">Reiniciar Juego</button>
+
+    <div class="row">
+      <div class="col-md-4"></div>
+      
+      <div class="col-md-4">
+      <center>
+        <div id="board">
+          <div class="cells player"></div>
+          <div class="cells player"></div>
+          <div class="cells player"></div>
+          <div class="cells player"></div>
+          <div class="cells player"></div>
+          <div class="cells player"></div>
+          <div class="cells player"></div>
+          <div class="cells player"></div>
+          <div class="cells player"></div>
+          <button id="reset-button">Reiniciar Juego</button>
+        </div>
+      </center>
       </div>
-    </center>
+
+      <div class="col-md-4"></div>
+
+    </div>
   </div>
   <div class="col-12">
     @foreach ($getheroes as $heroes)
@@ -52,6 +62,7 @@
     align-items: center;
     font-size: 60px;
     cursor: pointer;
+    background-size: cover;
   }
 
   .player-x {
@@ -67,90 +78,95 @@
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 
 <script>
-  const cells = document.querySelectorAll(".cells");
+ const cells = document.querySelectorAll(".cells");
+const heroButtons = document.querySelectorAll(".heroes");
 
-  const winConditions = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6]
-  ];
+const winConditions = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
 
-  function Player(name, token, url) {
-    this.name = name;
-    this.token = token;
-    this.url = url;
-  }
+function Player(name, token, url) {
+  this.name = name;
+  this.token = token;
+  this.url = url;
+}
 
-  const player1 = new Player("ivan", "X", '<img style="width: inherit" src="{{url('img/heros_img/90b2b001-072d-4e42-8bb7-4a0346c4834c.png')}}">');
-  const player2 = new Player("manuel", "O", '<img style="width: inherit" src="{{url('img/heros_img/795773be-63a5-4d48-83d1-fb6a2a744ce6.png')}}">');
+let player1 = null;
+let player2 = null;
+let gameEnded = false;
+let currentPlayer = null;
+let cellOccupied = Array(9).fill(false);
 
+heroButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    if (player1 === null) {
+      player1 = new Player("ivan", "X", button.style.backgroundImage);
+      currentPlayer = player1;
+      button.style.display = "none"; // Oculta el botón del héroe seleccionado
+    } else if (player2 === null) {
+      player2 = new Player("manuel", "O", button.style.backgroundImage);
+      button.style.display = "none"; // Oculta el botón del héroe seleccionado
+    }
+  });
+});
 
-  let gameEnd = false;
-  let currentPlayer = player1
-  cells.forEach(cell => {
-    cell.addEventListener("click", () => {
-      if (gameEnd) {
-        return;
-      }
-      if (cell.innerHTML === "") {  
+cells.forEach((cell, index) => {
+  cell.addEventListener("click", () => {
+    if (gameEnded || cellOccupied[index]) {
+      return;
+    }
 
-        cell.innerHTML = currentPlayer.url;
+    cell.style.backgroundImage = currentPlayer.url;
+    cellOccupied[index] = true;
 
-        if (checkWin()) {
-          gameEnd = true;
-          alert(currentPlayer.name + ' es el ganador!');
-          showResetButton(); // Muestra el botón de reinicio
-        } else if (checkTie()) {
-          gameEnd = true;
-          alert("Excelente juego, es un empate!");
-          showResetButton(); // Muestra el botón de reinicio
-        }
-        if (currentPlayer == player1) {
-          currentPlayer = player2
-        } else {
-          currentPlayer = player1
-        }
-      }
+    if (checkWin()) {
+      gameEnded = true;
+      alert(currentPlayer.name + ' es el ganador!');
+      showResetButton();
+    } else if (checkTie()) {
+      gameEnded = true;
+      alert("Excelente juego, es un empate!");
+      showResetButton();
+    }
+    currentPlayer = currentPlayer === player1 ? player2 : player1;
+  });
+});
+
+const resetButton = document.getElementById("reset-button");
+resetButton.addEventListener("click", () => {
+  resetGame();
+});
+
+function checkWin() {
+  return winConditions.some(condition => {
+    return condition.every(index => {
+      return cellOccupied[index] && cells[index].style.backgroundImage === currentPlayer.url;
     });
   });
+}
 
-  // Manejador de eventos para el botón de reinicio
-  const resetButton = document.getElementById("reset-button");
-  resetButton.addEventListener("click", () => {
-    resetGame(); // Llama a la función para reiniciar el juego
+function checkTie() {
+  return cellOccupied.every(occupied => occupied);
+}
+
+function showResetButton() {
+  resetButton.style.display = "block";
+}
+
+function resetGame() {
+  cells.forEach((cell, index) => {
+    cell.style.backgroundImage = "";
+    cellOccupied[index] = false;
   });
-
-  function checkWin() {
-    return winConditions.some(condition => {
-      return condition.every(index => {
-        return cells[index].innerHTML == currentPlayer.url;
-      });
-    });
-  }
-
-  function checkTie() {
-    return Array.from(cells).every(cell => {
-      return cell.innerHTML !== "";
-    });
-  }
-
-  // Función para mostrar el botón de reinicio
-  function showResetButton() {
-    resetButton.style.display = "block";
-  }
-
-  // Función para reiniciar el juego
-  function resetGame() {
-    cells.forEach(cell => {
-      cell.innerHTML = "";
-    });
-    currentPlayer = player1;
-    gameEnd = false;
-    resetButton.style.display = "none"; // Oculta el botón de reinicio
-  }
+  currentPlayer = player1;
+  gameEnded = false;
+  resetButton.style.display = "none";
+}
 </script>
